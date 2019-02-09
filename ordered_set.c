4acb80a7ec6723c57681cd1cc7129391334160a2
almiavicas <almiavicas@gmail.com>
2019-02-09 12:44:30 -0400

    Defined functions firms

--- ordered_set.c
+++ ordered_set.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+#include "ordered_set.h"
 
+err_mem = "no available memory";
+
+struct ordered_set * new_ordered_set(int size) {
+	struct ordered_set *set = malloc(sizeof(struct ordered_set) * size);
+	if (set == NULL) {
+		fprintf(stderr, "%s\n", err_mem);
+		perror("Error: ");
+		return NULL;
+	}
+	set->is_empty = is_empty;
+	set->get = get;
+	set->resize = resize;
+	set->insert = insert;
+	set->compare = compare;
+	return set;
+}
+
+int is_empty(void);
+void * get(int key);
+void resize(int size);
+void insert(int key, void * value);
+int compare(int key1, int key2);
 
 
--- ordered_set.h
+++ ordered_set.h
@@ -1,9 +1,28 @@
+/** Ordered set with insert operations */
+
+char[] err_mem;
+
 struct ordered_set {
-	void * begin;
-	void * end;
+	int * begin;
+	int * end;
 	int size;
-	int isEmpty(void);
-	void * at(int p);
-	void resize(int size);
+	void (*is_empty) (void);
+	void (*get) (int key);
+	void (*resize) (int size);
+	void (*insert) (int key, void (*value));
+	void (*compare) (int key1, int key2);
+
+	/** Represents an item inside the set */
+	struct item {
+		int key;
+		void * value;
+	};
 };
-ordered_set new_ordered_set(int size);
+
+struct ordered_set * new_ordered_set(int size);
+
+int is_empty(void);
+void * get(int key);
+void resize(int size);
+void insert(int key, void * value);
+int compare(int key1, int key2);
