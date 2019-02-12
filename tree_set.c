a488062d57ef70a50f27d570178ce01a0470c186
almiavicas <almiavicas@gmail.com>
2019-02-12 16:41:23 -0400

    Implementing red-black tree_set structure

--- tree_set.c
+++ tree_set.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h> // Included to use malloc
-#include "ordered_set.h"
+#include "tree_set.h"
 
 char err_mem[] = "not enought memory";
 char err_emp[] = "set is empty";
@@ -8,68 +8,102 @@ char err_knf[] = "key not found";
 char err_ful[] = "set is full";
 char err_rep[] = "element key is repeated";
 
-struct ordered_set * new_ordered_set(int item_size, int size) {
-	struct ordered_set *set = malloc(sizeof(struct ordered_set));
+struct tree_set * new_tree_set(int item_size, int max_size) {
+	struct tree_set *set = malloc(sizeof(struct tree_set));
 	if (set == NULL) {
 		fprintf(stderr, "%s\n", err_mem);
 		perror("Error: ");
 		return NULL;
 	}
+	int size = 2;
+	while (size <= max_size) size <<= 1;
+	size <<= 1; // Need an extra height to deal with insert_fixup
 	set->begin = malloc(sizeof(struct key_value) * size);
 	if (set->begin == NULL) {
 		fprintf(stderr, "%s\n", err_mem);
 		perror("Error: ");
+		free(set);
 		return NULL;	
 	}
-	set->end = set->begin;
-	set->size = size;
+	set->begin->color = BLACK; // begin is going to be used as the null node.
+	set->begin->parent = set->begin;
+	set->begin->left = set->begin + 1;
+	set->begin->right = set->begin + 1;
+	set->size = 0;
 	set->item_size = item_size;
+	set->max_size = size/2;
 	set->is_empty = &is_empty;
 	set->get = &get;
-	set->resize = &resize;
 	set->insert = &insert;
+	set->remove = &rb_remove;
+	build_tree(set, set->begin + 1);
 	return set;
 }
 
-static int is_empty(struct ordered_set *set) {
-	return set->begin == set->end;
+
+static void build_tree(struct tree_set * set, struct key_value * node) {
+	if (node == set->begin) return;
+	int id = node - set->begin;
+	node->parent = set->begin + id/2;
+	node->left = set->begin + left_child(set, id);
+	node->right = set->begin + right_child(set, id);
+	build_tree(set, node->left);
+	build_tree(set, node->right);
 }
 
-static void * get(struct ordered_set *set, int key) {
+static int left_child(struct tree_set * set, int n) {
+	n <<= 1;
+	return n > set->max_size ? 0 : n;
+}
+
+static int right_child(struct tree_set * set, int n) {
+	n = (n << 1) | 1;
+	return n > set->max_size ? 0 : n;
+}
+
+static int is_empty(struct tree_set *set) {
+	return set->size == 0;
+}
+
+static void * get(struct tree_set *set, int key) {
 	if (is_empty(set)) {
 		fprintf(stderr, "%s\n", err_emp);
 		return NULL;
 	}
-	struct key_value * left = set->begin;
-	struct key_value * right = set->end;
-	struct key_value * mid;
-	while (left < right) {
-		int dist = right - left;
-		mid = left + (dist / 2);
-		if (mid->key < key) {
-			left = mid + 1;
+	struct key_value * x = set->begin + 1;
+	while (x != set->begin) {
+		if (x->key == key) {
+			return x->value;
 		}
-		else if (mid->key > key) {
-			right = mid;
+		else if (x->key < key) {
+			x = x->right;
 		}
-		else break;
+		else {
+			x = x->left;
+		}
 	}
-	if (mid == NULL || mid->key != key) {
-		fprintf(stderr, "%s: %d\n", err_knf, key);
-		return NULL;
-	}
-	return mid->value;
+	fprintf(stderr, "%s: %d\n", err_knf, key);
+	return NULL;
 }
 
-static void resize(struct ordered_set * set, int size) {
+static void resize(struct tree_set * set, int max_size) {
+	int size = 2;
+	while (size <=max_size) size <<=1;
+	size<<=1; // Needed to deal with insert_fixup
+	if (size/2 == set->max_size) return;
 	struct key_value * new_begin = malloc(sizeof(struct key_value) * size);
 	if (new_begin == NULL) {
 		fprintf(stderr, "%s\n", err_mem);
 		return;
 	}
+	int old_size = set->max_size;
+	set->max_size = size/2;
 	struct key_value * it = new_begin;
 	struct key_value * old_begin = set->begin;
-	while ((old_begin < set->end) && (it - new_begin < size)) {
+	while (it-new_begin <= max_size && old_begin-set->begin <= old_size) {
+		it->left = old_begin->left;
+		it->right = old_begin->right;
+		it->parent = old_begin->parent;
 		it->key = old_begin->key;
 		it->value = old_begin->value;
 		it++;
@@ -77,43 +111,139 @@ static void resize(struct ordered_set * set, int size) {
 	}
 	free(set->begin);
 	set->begin = new_begin;
-	set->end = it;
-	set->size = size;
 }
 
-static int insert(struct ordered_set * set, int key, void * value) {
-	if (set->end - set->begin == set->size) {
-		fprintf(stderr, "%s\n", err_ful);
-		return 0;
-	}
-	struct key_value *left = set->begin;
-	struct key_value *mid = left;
-	struct key_value *right = set->end;
-	while (left < right) {
-		int dist = right - left;
-		mid = left + (dist / 2);
-		if (mid->key < key) {
-			left = mid + 1;
+static int insert(struct tree_set * set, int key, void * value) {
+	struct key_value * y = set->begin; // null node
+	struct key_value * x = y + 1; // root
+	while (x != set->begin) {
+		y = x;
+		if (key < x->key) {
+			x = x->left;
 		}
-		else if (mid->key > key) {
-			right = mid;
+		else {
+			x = x->right;
 		}
-		else break;
 	}
-	if (mid->key == key) {
-		fprintf(stderr, "%s\n", err_rep);
-		return 0;
+	if (y->parent == set->begin) {
+		x = set->begin+1;
+		x->key = key;
+		x->value = value;
 	}
-	struct key_value *it = set->end;
-	while (mid < it) {
-		it--;
-		(it+1)->key = it->key;
-		(it+1)->value = it->value;
+	else if (key < y->key) {
+		x = y->left;
+		x->key = key;
+		x->value = value;
 	}
-	mid->key = key;
-	mid->value = value;
-	set->end++;
+	else {
+		x = y->right;
+		x->key = key;
+		x->value = value;
+	}
+	x->left = set->begin;
+	x->right = set->begin;
+	x->color = RED;
+	insert_fixup(set, x);
 	return 1;
 }
 
+static void insert_fixup(struct tree_set *set, struct key_value * node) {
+	struct key_value * y;
+	while (node->parent->color == RED) {
+		if (node->parent == node->parent->parent->left) {
+			y = node->parent->parent->right;
+			if (y->color == RED) {
+				node->parent->color = BLACK;
+				y->color = BLACK;
+				node->parent->parent->color = RED;
+				node = node->parent->parent;
+			}
+			else {
+				if (node == node->parent->right) {
+					node = node->parent;
+					left_rotate(set, node);
+				}
+				node->parent->color = BLACK;
+				node->parent->parent->color = RED;
+				right_rotate(set, node->parent->parent);
+			}
+		}
+		else {
+			y = node->parent->parent->left;
+			if (y->color == RED) {
+				node->parent->color = BLACK;
+				y->color = BLACK;
+				node->parent->parent->color = RED;
+				node = node->parent->parent;
+			}
+			else {
+				if (node == node->parent->left) {
+					node = node->parent;
+					right_rotate(set, node);
+				}
+				node->parent->color = BLACK;
+				node->parent->parent->color = RED;
+				left_rotate(set, node->parent->parent);
+			}
+		}
+	}
+	(set->begin + 1)->color = BLACK;
+}
 
+static int rb_remove(struct tree_set * set, int key) {
+
+	return 0;
+}
+
+static void transplant(struct tree_set * set, struct key_value * u, struct key_value * v) {
+
+}
+
+
+static void left_rotate(struct tree_set *set, struct key_value * node) {
+	struct key_value * y = node->right;
+	node->right = y->left;
+	if (y->left != set->begin) {
+		y->left->parent = node;
+	}
+	y->parent = node->parent;
+	if (node->parent == set->begin) {
+		copy(y, set->begin+1);
+	}
+	else if (node == node->parent->left) {
+		node->parent->left = y;
+	}
+	else {
+		node->parent->right = y;
+	}
+	y->left = node;
+	node->parent = y;
+}
+
+static void right_rotate(struct tree_set *set, struct key_value * node) {
+	struct key_value * y = node->left;
+	node->left = y->right;
+	if (y->right != set->begin) {
+		y->left->parent = node;
+	}
+	y->parent = node->parent;
+	if (node->parent == set->begin) {
+		copy(y, set->begin+1);
+	}
+	else if (node == node->parent->right) {
+		node->parent->right = y;
+	}
+	else {
+		node->parent->left = y;
+	}
+	y->right = node;
+	node->parent = y;
+}
+
+static void copy(struct key_value * from, struct key_value * to) {
+	to->parent = from->parent;
+	to->left = from->left;
+	to->right = from->right;
+	to->key = from->key;
+	to->value = from->value;
+}
--- tree_set.h
+++ tree_set.h
@@ -1,4 +1,7 @@
-/** Ordered set with insert operations */
+/** RED-BLACK tree set with insert operations */
+
+#define BLACK 0
+#define RED 1
 
 char err_mem[];
 char err_emp[];
@@ -6,26 +9,41 @@ char err_knf[];
 char err_ful[];
 char err_rep[];
 
-/** Represents an item inside the set */
+/** Represents a node inside the tree */
 struct key_value {
 	int key;
 	void * value;
+	struct key_value * left;
+	struct key_value * right;
+	struct key_value * parent;
+	unsigned int color : 1;
 };
 
-struct ordered_set {
+struct tree_set {
 	struct key_value * begin;
-	struct key_value * end;
 	int item_size;
 	int size;
-	int (*is_empty) (struct ordered_set *set);
-	void *(*get) (struct ordered_set *set, int key);
-	void (*resize) (struct ordered_set *set, int size);
-	int (*insert) (struct ordered_set *set, int key, void (*value));
+	int max_size;
+	int (*is_empty) (struct tree_set *set);
+	void *(*get) (struct tree_set *set, int key);
+	void (*resize) (struct tree_set *set, int size);
+	int (*insert) (struct tree_set *set, int key, void (*value));
+	int (*remove) (struct tree_set * set, int key);
 };
 
-struct ordered_set * new_ordered_set(int item_size, int size);
+struct tree_set * new_tree_set(int item_size, int size);
 
-static int is_empty(struct ordered_set *set);
-static void * get(struct ordered_set *set, int key);
-static void resize(struct ordered_set *set, int size);
-static int insert(struct ordered_set *set, int key, void * value);
+static void build_tree(struct tree_set * set, struct key_value * node);
+static int left_child(struct tree_set * set, int n);
+static int right_child(struct tree_set * set, int n);
+static int is_empty(struct tree_set *set);
+static void * get(struct tree_set *set, int key);
+static int * keys(struct tree_set *set);
+static void resize(struct tree_set *set, int size);
+static int insert(struct tree_set *set, int key, void * value);
+static void insert_fixup(struct tree_set *set, struct key_value * node);
+static int rb_remove(struct tree_set * set, int key);
+static void transplant(struct tree_set * set, struct key_value * u, struct key_value * v);
+static void left_rotate(struct tree_set *set, struct key_value * node);
+static void right_rotate(struct tree_set *set, struct key_value * node);
+static void copy(struct key_value * from, struct key_value * to);
